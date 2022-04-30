4013d6967f0e06febcaa6d0c597fdd665653ee37
Isaac Dew <lildew2@gmail.com>
2022-04-30 16:32:17 -0600

    Add publish tag

--- src/TenantServiceProvider.php
+++ src/TenantServiceProvider.php
@@ -17,7 +17,7 @@ class TenantServiceProvider extends ServiceProvider
             __DIR__ . '/../publish/migrations/2022_04_30_214344_create_tenants_table.php' => base_path('/database/migrations/2022_04_30_214344_create_tenants_table'),
             __DIR__ . '/../publish/migrations/2022_04_30_215100_add_tenant_id_to_users_table.php' => base_path('/database/migrations/2022_04_30_215100_add_tenant_id_to_users_table.php'),
             __DIR__ . '/../publish/models/Tenant.php' => app_path('/Models/migrations/Tenant.php'),
-        ]);
+        ], 'laravel-fortify-multitenancy-migrations');
 
         Fortify::authenticateUsing(function (Request $request) {
             $user = User::where('email', $request->email)
