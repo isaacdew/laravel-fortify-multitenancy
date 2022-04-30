e43087eb6ddc3b47ffcdd885aec807e1287d653c
Isaac Dew <lildew2@gmail.com>
2022-04-30 16:38:08 -0600

    Add missing .php

--- src/TenantServiceProvider.php
+++ src/TenantServiceProvider.php
@@ -14,7 +14,7 @@ class TenantServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/../publish/migrations/2022_04_30_214344_create_tenants_table.php' => base_path('/database/migrations/2022_04_30_214344_create_tenants_table'),
+            __DIR__ . '/../publish/migrations/2022_04_30_214344_create_tenants_table.php' => base_path('/database/migrations/2022_04_30_214344_create_tenants_table.php'),
             __DIR__ . '/../publish/migrations/2022_04_30_215100_add_tenant_id_to_users_table.php' => base_path('/database/migrations/2022_04_30_215100_add_tenant_id_to_users_table.php'),
             __DIR__ . '/../publish/models/Tenant.php' => app_path('/Models/migrations/Tenant.php'),
         ], 'laravel-fortify-multitenancy-migrations');
