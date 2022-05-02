0bcdf2c6d7013fd57f0e8a4d29a474576ff9681d
Isaac Dew <lildew2@gmail.com>
2022-05-01 21:05:00 -0600

    Start setup of tests

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Isaacdew\LaravelFortifyMultitenancy\Tests;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    protected $loadEnvironmentVariables = true;
+
+    // 
+}  
